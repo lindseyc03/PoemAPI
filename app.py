f3c46681dd827468338f4c523ed5d2a5875511fd
lindsxcr03 <lindsey.cr123@gmail.com>
2023-08-06 19:02:02 -0700

    Added poem history functionality

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///poemshistory.db'  # Change the database URI if needed
+db = SQLAlchemy(app)
+
+class Poem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), nullable=False)
+    author = db.Column(db.String(128), nullable=False)
+    lines = db.Column(db.Text, nullable=False)
+
+# Move db.create_all() within the app context
+with app.app_context():
+    db.create_all()
 
 # Function to fetch random poems from the PoetryDB API
 def fetch_random_poems(num_poems):
@@ -26,10 +39,19 @@ def home():
         try:
             num_poems = int(request.form.get('num_poems', 1))
             poems = fetch_random_poems(num_poems)
+            for poem in poems:
+                poem_entry = Poem(title=poem['title'], author=poem['author'], lines='\n'.join(poem['lines']))
+                db.session.add(poem_entry)
+            db.session.commit()
         except ValueError:
             pass
 
     return render_template('home.html', poems=poems)
+
+@app.route('/poem_history')
+def poem_history():
+    poems = Poem.query.all()
+    return render_template('poem_history.html', poems=poems)
 
 if __name__ == '__main__':
     app.run(debug=True)
