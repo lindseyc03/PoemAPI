1788999873f0e67849056369836f30f9aefb45bb
lindsxcr03 <lindsey.cr123@gmail.com>
2023-08-06 19:27:33 -0700

    Cleaning up

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import requests
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///poemshistory.db'  # Change the database URI if needed
 db = SQLAlchemy(app)
+migrate = Migrate(app, db) 
 
 class Poem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), nullable=False)
     author = db.Column(db.String(128), nullable=False)
     lines = db.Column(db.Text, nullable=False)
+    linecount = db.Column(db.Integer, nullable=True) 
 
 # Move db.create_all() within the app context
 with app.app_context():
@@ -40,7 +44,7 @@ def home():
             num_poems = int(request.form.get('num_poems', 1))
             poems = fetch_random_poems(num_poems)
             for poem in poems:
-                poem_entry = Poem(title=poem['title'], author=poem['author'], lines='\n'.join(poem['lines']))
+                poem_entry = Poem(title=poem['title'], author=poem['author'], lines='\n'.join(poem['lines']), linecount=poem['linecount'])
                 db.session.add(poem_entry)
             db.session.commit()
         except ValueError:
